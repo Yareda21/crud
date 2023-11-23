0cf8569c9806cf39a2ab61fc18b533d39e676746
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2023-11-23 10:05:58 +0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/nav";
+import Nav from "@/components/Nav";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
