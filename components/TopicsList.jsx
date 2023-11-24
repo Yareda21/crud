0c84a125f74921231d79695212e9cdff7e6a571e
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2023-11-24 12:18:47 +0300

    update

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -5,7 +5,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch("http://yared-crud.vercel.app/api/topics", {
       cache: "no-store",
     });
     if (!res.ok) {
